7b6c90568a7a9289a16e5a05e8af1da039edf11d
victor <victorugwueze@gmail.com>
2019-12-05 17:53:26 +0100

    copy files to instance

--- scripts/travis-build-deploy.sh
+++ scripts/travis-build-deploy.sh
@@ -10,7 +10,7 @@ source $ROOT_DIR/scripts/utils.sh
 # }
 
 sshToInstance() {
-  gcloud compute scp travis-app-vm --recurse $TRAVIS_BUILD_DIR:~/
+  gcloud compute scp travis-app-vm --recurse $TRAVIS_BUILD_DIR:travis-app-vm/
 }
 
 
