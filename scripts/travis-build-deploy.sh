2f6112052367aa841ccf0e80aa2d61ec9b425bf3
victor <victorugwueze@gmail.com>
2019-12-05 21:30:34 +0100

    install nginx server

--- scripts/travis-build-deploy.sh
+++ scripts/travis-build-deploy.sh
@@ -12,6 +12,8 @@ source $ROOT_DIR/scripts/utils.sh
 sshToInstance() {
   ls $TRAVIS_BUILD_DIR/
   gcloud compute scp --recurse $TRAVIS_BUILD_DIR/  travis-app-vm:~
+  sudo apt-get update
+  sudo apt-get install nginx-light -y
 }
 
 
