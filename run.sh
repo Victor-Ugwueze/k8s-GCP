2bc5af7aff966e297a7ad6ec85a23087811f2d2f
Victor Ugwueze <victorugwueze@gmail.com>
2020-04-12 14:55:16 +0100

    change image to google/cloud-sdk

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yes | gcloud compute ssh victorugwueze$COMPUTE_INSTANCE_NAME --command 'cd /k8s-GCP && git pull && ls && exit'
+yes | gcloud compute ssh victorugwueze@$COMPUTE_INSTANCE_NAME --command 'cd /k8s-GCP && git pull && ls && exit'
