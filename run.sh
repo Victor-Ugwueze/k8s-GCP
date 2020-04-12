58e1e33cd5c645acf5cdb591d35f1aac1e868c85
Victor Ugwueze <victorugwueze@gmail.com>
2020-04-12 16:30:08 +0100

    change port

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 yes | gcloud compute ssh victorugwueze@$COMPUTE_INSTANCE_NAME \
---command 'cd /k8s-GCP && sudo chmod +x ./start.sh && && sudo ./start.sh && exit'
+--command 'cd /k8s-GCP && sudo chmod +x ./start.sh && sudo ./start.sh && exit'
